1d7eb5421b66bf392905af5144ffa7c5f848a2ea
This is Abishekh <abishekhrajendhran111@gmail.com>
2024-01-09 09:03:36 +0530

    sum of natural numbers program

---
+++ src/practice/basic/Sum_natural_numbers.java
@@ -0,0 +1,29 @@
+package practice.basic;
+
+import java.util.Scanner;
+
+public class Sum_natural_numbers {
+	
+	static int sum(int start, int end ) {
+		int add = 0;
+		for (int i = start; i <=end; i++) {
+			add = add + i;
+		}
+		return add;
+		
+	}
+	
+	public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
+		
+		System.out.println("Enter number_1 = ");
+		int A = inputs.nextInt();
+		
+		System.out.println("Enter number_2 = ");
+		int B = inputs.nextInt();
+		
+		System.out.printf("Sum of natural numbers between %d and %d are %d", A,B,sum(A, B));
+		
+	}
+
+}
