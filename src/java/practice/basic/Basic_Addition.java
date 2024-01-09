8091cf08bc937a98d30f3b011a96a445d344178d
This is Abishekh <abishekhrajendhran111@gmail.com>
2024-01-09 08:56:56 +0530

    Package deleted and new file have added!

--- src/java/practice/basic/Basic_Addition.java
+++
@@ -1,8 +0,0 @@
-package java.practice.basic;
-
-public class Basic_Addition {
-
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ src/practice/basic/Basic_Addition.java
@@ -0,0 +1,27 @@
+package practice.basic;
+
+import java.util.Scanner;
+
+public class Basic_Addition {
+	
+	static int add(int a, int b) {
+		int sum = a + b;
+		return sum;
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner inputs = new Scanner(System.in);
+		
+		System.out.println("Enter number_1 = ");
+		int A = inputs.nextInt();
+		
+		System.out.println("Enter number_2 = ");
+		int B = inputs.nextInt();
+		
+		System.out.printf("Addition of two numbers %d and %d are %d", A,B,add(A, B));
+		
+		
+		
+	}
+}
